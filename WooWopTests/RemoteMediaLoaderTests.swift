e9a59e131988506a29d7fb4a329e6508561ffe5d
10202018 <44626394+10202018@users.noreply.github.com>
2024-05-21 22:37:38 -0700

    Refactor tests to remove duplication.

--- WooWopTests/RemoteMediaLoaderTests.swift
+++ WooWopTests/RemoteMediaLoaderTests.swift
@@ -8,15 +8,23 @@
 import ShazamKit
 import XCTest
 
+extension SHManagedSession: Equatable {
+  public static func == (lhs: SHManagedSession, rhs: SHManagedSession) -> Bool {
+    return (lhs.self === rhs.self)
+  }
+}
+
 class RemoteMediaLoader {
   let client: HTTPClient
+  let session: SHManagedSession
   
-  init(client: HTTPClient) {
+  init(client: HTTPClient, session: SHManagedSession) {
     self.client = client
+    self.session = session
   }
   
   func load() {
-    client.get(from: SHManagedSession())
+    client.get(from: session)
   }
 }
 
@@ -36,10 +44,21 @@ class HTTPClientSpy: HTTPClient {
 final class RemoteMediaLoaderTests: XCTestCase {
 
   func test_init_doesNotRequestMatchFromSession() {
+    let session = SHManagedSession()
     let client = HTTPClientSpy()
-    _ = RemoteMediaLoader(client: client)
+    _ = RemoteMediaLoader(client: client, session: session)
     
     XCTAssertNil(client.requestedShazamSession)
+  }
+  
+  func test_load_requestMatchFromSession() {
+    let session = SHManagedSession()
+    let client = HTTPClientSpy()
+    let sut = RemoteMediaLoader(client: client, session: session)
+    
+    sut.load()
+    
+    XCTAssertEqual(client.requestedShazamSession, session)
   }
 
 }
