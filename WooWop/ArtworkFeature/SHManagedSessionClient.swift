426e6c144bbb7dcfe92f604048aaa63543a71b1d
10202018 <44626394+10202018@users.noreply.github.com>
2024-08-13 12:50:08 -0700

    Organize files into modules.

--- WooWop/ArtworkFeature/SHManagedSessionClient.swift
+++ WooWop/ArtworkFeature/SHManagedSessionClient.swift
@@ -33,30 +33,3 @@ public class SHManagedSessionClient: Client {
     }
   }
 }
-
-//class ShazamClient {
-//  
-//  // Set up the session
-//  /// An object that records and matches a recording with captured sound in the Shazam catalog or your
-//  /// custom catalog.
-//  private var shazamSession = SHManagedSession()
-//  
-//  /// Makes  an asynchrnous request to match the signature (hashed version passed to and from client) of
-//  /// the song from the current session.
-//  /// Returns:
-//  /// - an object that represents the metadata for a reference signature.
-//  func executeSessionAndMatch() async -> SHSession.Result {
-//    return await shazamSession.result()
-//  }
-//  
-//  func getSessionResult() async -> SHMediaItem? {
-//    // Use the result.
-//    let shazamSession = SHManagedSession()
-//    switch await shazamSession.result() {
-//    case .match(let match):
-//      return match.mediaItems.first
-//    case .noMatch(_): return nil
-//    case .error(let error, _): return nil
-//    }
-//  }
-//}
